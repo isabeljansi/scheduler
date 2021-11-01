2177acc62f40b9f70c62a01e41dacb1adb3bd6dc
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-11-01 19:27:13 -0400

    add the show/hide form logic

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,9 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+import useVisualMode from "hooks/useVisualMode";
 
 import './styles.scss';
 
@@ -13,13 +16,27 @@ export default function Appointment(props){
 
   const { time, interview } = props
 
-  
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  // Set the interviewers prop to an empty array. We will implement this properly in a future activity.
+  const interviewers = [];
+
+const { mode, transition, back } = useVisualMode(
+  interview ? SHOW : EMPTY
+);
 
   return(
     <article className="appointment">
       <Header time={time}
       /> 
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty /> }
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (<Show student={interview.student} interviewer={interview.interviewer }/>)}
+      {mode === CREATE && (<Form interviewers={interviewers} onCancel={back}/>)}
+
+      {/* {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty /> } */}
       
     </article>
 
