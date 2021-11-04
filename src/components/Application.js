8968164aab8b476be718c755c8169575283f7d60
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-11-04 16:25:28 -0400

    clean up code

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,66 +21,9 @@ export default function Application(props) {
 
   const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
-  // const [state, setState] = useState({
-  //   day: "Monday",
-  //   days: [],
-  //   appointments: {}
-  // });
-
-  // const setDay = day => setState({ ...state, day }); //used to update DayList
-
-  // useEffect(() => {
-  //   Promise.all([
-  //     axios.get("/api/days"),
-  //     axios.get("/api/appointments"),
-  //     axios.get("/api/interviewers"),
-  //   ]).then((all) => {
-  //     console.log(all);
-  //     setState((prev) => ({
-  //       ...prev,
-  //       days: all[0].data,
-  //       appointments: all[1].data,
-  //       interviewers: all[2].data,
-  //     }));
-  //   });
-  // }, []);
-
   const interviewers = getInterviewersForDay(state, state.day);
 
   const appointments = getAppointmentsForDay(state, state.day);
-
-  // function bookInterview(id, interview) {
-  //   console.log(id, interview);
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: { ...interview }
-  //   };
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   };
-  //   setState({
-  //     ...state,
-  //     appointments
-  //   });
-  //   return axios.put(`/api/appointments/${id}`, {interview})
-  // }
-
-  // function cancelInterview(id){
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: null
-  //   };
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   };
-  //   setState({
-  //     ...state,
-  //     appointments,
-  //   });
-  //   return axios.delete(`/api/appointments/${id}`)
-  // }
 
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
